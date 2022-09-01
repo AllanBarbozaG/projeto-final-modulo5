ae1b206664b5c33c358669bbb9d9ba79e2421698
allanbarbozag <allpatrick.b@gmail.com>
2022-09-01 19:09:56 -0300

    build: adiciona arquivos iniciais do projeto.

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Login from './components/Login/Login'
+
+function App() {
+  
+
+  return (
+
+    <div className="App">
+     
+      
+     <Login />
+    </div>
+  )
+}
+
+export default App
