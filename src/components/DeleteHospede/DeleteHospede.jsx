27c0d4e46e07a1c8be838085b43b86fb8a392ddb
allanbarbozag <allpatrick.b@gmail.com>
2022-09-02 21:49:46 -0300

    feat: Adiciona função para deleção de hóspedes ao arquivo requestHospedes.js.

---
+++ src/components/DeleteHospede/DeleteHospede.jsx
@@ -0,0 +1,12 @@
+import { DeleteRequest } from "../../service/requestHospedes";
+import { useEffect } from 'react'
+
+function DeleteHospede() {
+  useEffect(() => {
+    DeleteRequest()
+      .then((json) => console.log(json))
+      .catch((error) => console.log(error + "deu erro"));
+  }, []);
+}
+
+export default DeleteHospede;
