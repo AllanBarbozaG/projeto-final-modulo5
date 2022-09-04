9477580391890adc338b49c0efd49180b1021957
letxns <lele_martins1999@yahoo.com.br>
2022-09-04 02:23:13 -0300

    build: cria rotas para pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { useState } from "react"
 import Home from "./Pages/Home/"
 import Reservas from "./Pages/Reservas/"
+import Quartos from "./Pages/Quartos/"
+import Hospedes from "./Pages/Hospedes"
+import Funcionarios from "./Pages/Funcionarios"
+import Limpezas from "./Pages/Limpezas"
+import Login from "./Pages/Login"
 import Layout from "./components/Layout"
 import style from "./App.module.css"
 
@@ -14,6 +19,11 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/reservas" element={<Reservas />} />
+            <Route path="/Quartos" element={<Quartos />} />
+            <Route path="/Hospedes" element={<Hospedes />} />
+            <Route path="/Funcionarios" element={<Funcionarios />} />
+            <Route path="/Limpezas" element={<Limpezas />} />
+            <Route path="/Login" element={<Login />} />
           </Routes>
         </Layout>
       </BrowserRouter>
