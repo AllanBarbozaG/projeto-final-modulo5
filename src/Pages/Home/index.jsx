8cce44e4f51b9e20df1fbc3b6623314e32b2ac5a
Ramom Ferraz <ramomferraz@hotmail.com>
2022-09-03 21:51:22 -0300

    Build: Cria nova pagina 'Home'

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,16 +1,28 @@
-import style from './home.module.css'
+import "./Home.css"
 
 function Home() {
     return (
-        <div className={style.container}>
-            <div className={style.loader}>
-                <h2>AGUARDANDO LOGIN</h2>
-                <div className={style.dot} />
-                <div className={style.dot} />
-                <div className={style.dot} />
-                <div className={style.dot} />
-            </div>
+        <div class="animation">
+            <h1>AGUARDANDO LOGIN</h1><br />
+            <p class="circle">
+                <span class="ouro">
+                    <span class="left"><span class="anim"></span></span>
+                    <span class="right"><span class="anim"></span></span>
+                </span>
+                <span class="ouro ouro2">
+                    <span class="left"><span class="anim"></span></span>
+                    <span class="right"><span class="anim"></span></span>
+                </span>
+
+                <span class="ouro ouro3">
+                    <span class="left"><span class="anim"></span></span>
+                    <span class="right"><span class="anim"></span></span>
+                </span>
+            </p>
         </div>
+
+
+
     )
 }
 
