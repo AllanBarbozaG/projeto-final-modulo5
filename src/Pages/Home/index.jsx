a4209d84142fcc3f61bdb56abfaea9b315f86bf1
Ramom <ramomferraz@hotmail.com>
2022-09-05 19:06:23 -0300

    refactor: altera erro de digitação

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -4,7 +4,7 @@ function Home() {
     return (
         <div class={style.container}>
             <div>
-                <p class={style.welcome}>Bem vindo!</p>
+                <p class={style.welcome}>Bem-vindo!</p>
                 <p class={style.text1}>Utilize o painel ao lado para realizar suas ações.</p>
                 <p class={style.text2}>Tenha um ótimo dia de trabalho.</p>
             </div>
