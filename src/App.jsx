5dd0747b64babe9a255eab158a00d403990821cc
Ramom Ferraz <ramomferraz@hotmail.com>
2022-09-05 14:46:59 -0300

    Cria função para checar se está logado

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,25 @@ import { useState } from "react"
 import Home from "./Pages/Home/"
 import Reservas from "./Pages/Reservas/"
 import Layout from "./components/Layout"
+import Login from "./Pages/Login"
 import style from "./App.module.css"
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false)
   return (
-    <div className={style.app}>
-      <BrowserRouter>
-        <Layout isAuthenticated={isAuthenticated} >
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/reservas" element={<Reservas />} />
-          </Routes>
-        </Layout>
-      </BrowserRouter>
-    </div>
+    <>
+      {isAuthenticated ?
+        <div className={style.app}>
+          <BrowserRouter>
+            <Layout isAuthenticated={isAuthenticated} >
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/reservas" element={<Reservas />} />
+              </Routes>
+            </Layout>
+          </BrowserRouter>
+        </div> : <Login setIsAuthenticated={setIsAuthenticated} />}
+    </>
   )
 }
 
