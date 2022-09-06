e7e4744b44767a719519d5f7a4fa838f91d996b4
Ramom <ramomferraz@hotmail.com>
2022-09-06 17:40:35 -0300

    Build: Cria página para adicionar reservas

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { useState } from "react"
 import Home from "./Pages/Home/"
 import Reservas from "./Pages/Reservas/"
+import NovaReserva from "./Pages/Reservas/novaReserva"
 import Quartos from "./Pages/Quartos/"
 import Hospedes from "/src/components/Hospedes/Hospedes"
 import Funcionarios from "./Pages/Funcionarios"
@@ -21,6 +22,7 @@ function App() {
               <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/reservas" element={<Reservas />} />
+                <Route path="/addreserva" element={<NovaReserva />} />
                 <Route path="/quartos" element={<Quartos />} />
                 <Route path="/hospedes" element={<Hospedes />} />
                 <Route path="/funcionarios" element={<Funcionarios />} />
