77697f8ba724c594474533966c8886387d5c540b
allanbarbozag <allpatrick.b@gmail.com>
2022-09-07 03:27:31 -0300

    style: adequa as funcionalidades da página da entidade hóspedes ao estilo pré definido.

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,13 @@ import Home from "./Pages/Home/"
 import Reservas from "./Pages/Reservas/"
 import NovaReserva from "./Pages/Reservas/novaReserva"
 import Quartos from "./Pages/Quartos/"
-import Hospedes from "/src/components/Hospedes/Hospedes"
+import Hospedes from "./Pages/Hospedes/index"
 import Funcionarios from "./Pages/Funcionarios/index";
 import Limpezas from "./Pages/Limpezas"
 import Login from "./Pages/Login"
 import Layout from "./components/Layout"
 import style from "./App.module.css"
+import PostHospedes from './components/Hospedes/PostHospedes/PostHospedes'
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false)
@@ -25,6 +26,7 @@ function App() {
                 <Route path="/addreserva" element={<NovaReserva />} />
                 <Route path="/quartos" element={<Quartos />} />
                 <Route path="/hospedes" element={<Hospedes />} />
+                <Route path="/addhospede" element={<PostHospedes />} />
                 <Route path="/funcionarios" element={<Funcionarios />} />
                 <Route path="/limpezas" element={<Limpezas />} />
               </Routes>
