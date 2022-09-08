ed7d4f3f353249aa60ce2ee52aa278eebbcc080e
allanbarbozag <allpatrick.b@gmail.com>
2022-09-08 01:05:00 -0300

    feat: implementa função de busca de hóspedes por cpf.

--- src/Pages/Hospedes/index.jsx
+++ src/Pages/Hospedes/index.jsx
@@ -10,16 +10,17 @@ function Hospedes() {
   const [hospedes, setHospedes] = useState([]);
   const [loadingReqData, setLoadingReqData] = useState(true);
   const [guestCpf, setGuestCpf] = useState();
-  console.log(guestCpf);
-  console.log(guestCpfIndex);
+  const [guestCpfSearch, setGuestCpfSearch] = useState();
+  console.log("gestcpfsearch: " + guestCpfSearch);
   const navigate = useNavigate();
 
   useEffect(() => {
     GetRequest().then((data) => {
+      console.log("req feita");
       setHospedes(data);
       setLoadingReqData(false);
     });
-  }, [loadingReqData]);
+  }, [loadingReqData, guestCpfSearch]);
 
   useEffect(() => {
     guestCpfIndex = [];
@@ -37,6 +38,105 @@ function Hospedes() {
           <div>CARREGANDO...</div>
           {console.log("carregando")}
         </>
+      ) : guestCpfSearch && guestCpfSearch.length == 11 ? (
+        <>
+          {/* Após verificar se há guestCpfSearch, irá exibir praticamente a mesma página, porém, apenas com o hospede procurado */}
+          {hospedes.map((hospede) => {
+            if (hospede.cpf == guestCpfSearch) {
+              return (
+                <>
+                  <div className={style.container}>
+                    <div className={style.title}>
+                      <h2>Controle de hóspedes</h2>
+                      <div className={style.reservation}>Total de hóspedes</div>
+                      <div className={style.checkin}>Hóspedes chegando</div>
+                      <div className={style.checkout}>Hóspedes saindo</div>
+                    </div>
+                    <div className={style.search}>
+                      <button
+                        type="button"
+                        onClick={() => handleSetPage("/addhospede")}
+                        className={style.buttonCreate}
+                      >
+                        Adicionar Hospede
+                      </button>
+                      <input
+                        onChange={(e) => setGuestCpfSearch(e.target.value)}
+                        type={"text"}
+                        value={guestCpfSearch}
+                        placeholder="Buscar CPF"
+                      />
+                    </div>
+
+                    <div className={style.containerTable}>
+                      <table className={style.table}>
+                        <thead className={style.thead}>
+                          <tr>
+                            <th scope="col" className={style.th}>
+                              CPF Hóspede
+                            </th>
+                            <th scope="col" className={style.th}>
+                              Nome Hóspede
+                            </th>
+                            <th scope="col" className={style.th}>
+                              número quarto
+                            </th>
+                            <th scope="col" className={style.th}>
+                              Telefone
+                            </th>
+
+                            <th scope="col" className={style.th}>
+                              ação
+                            </th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr className={style.body} key={hospede.cpf}>
+                            <td scope="row" className={style.linhas}>
+                              {hospede.cpf}
+                            </td>
+                            <td className={style.linhas}>{hospede.nome}</td>
+                            <td className={style.linhas}>
+                              {hospede.numeroQuarto}
+                            </td>
+                            <td className={style.linhas}>{hospede.telefone}</td>
+
+                            <td className={style.icons}>
+                              <img src="./view.png" />
+                              <img
+                                src="./lapis.png"
+                                onClick={(e) => {
+                                  setGuestCpf(hospede.cpf);
+                                  guestCpfIndex.push(hospede.cpf);
+                                  navigate("/updatehospede");
+                                }}
+                              />
+                              <img
+                                src="./lixeira.png"
+                                onClick={(e) => {
+                                  e.preventDefault;
+                                  const confirmation = confirm(
+                                    "Tem certeza de que deseja excluir o registro deste hóspede?Não será possível recuperar os dados."
+                                  );
+                                  if (confirmation == true) {
+                                    setGuestCpf(hospede.cpf);
+                                    guestCpfIndex.push(hospede.cpf);
+                                    deleteGuest = true;
+                                    navigate("/deletehospede");
+                                  }
+                                }}
+                              />
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </>
+              );
+            }
+          })}
+        </>
       ) : (
         <div className={style.container}>
           <div className={style.title}>
@@ -53,7 +153,11 @@ function Hospedes() {
             >
               Adicionar Hospede
             </button>
-            <input type={"text"} placeholder="Buscar CPF" />
+            <input
+              onChange={(e) => setGuestCpfSearch(e.target.value)}
+              type={"text"}
+              placeholder="Buscar CPF"
+            />
           </div>
 
           <div className={style.containerTable}>
