a053397dd7c49249fc95b0f763c4c2d2de15d595
Ramom <ramomferraz@hotmail.com>
2022-09-09 18:29:29 -0300

    Fix: Identação de código

--- src/components/Limpezas/PostLimpezas/PostLimpezas.jsx
+++ src/components/Limpezas/PostLimpezas/PostLimpezas.jsx
@@ -11,35 +11,36 @@ function PostLimpezas() {
     const [sendRequest, setSendRequest] = useState(false);
 
     useEffect(() => {
-    if (sendRequest) {
-        setSendRequest(false);
-        PostLimpezasRequest(roomNumber, isItClean, employeeId, cleanningDate)
-        .then((json) => {setSendRequest()
-            console.log(json);
-        })
-        .catch((error) => console.log(error + "deu erro"));
-    }
+        if (sendRequest) {
+            setSendRequest(false);
+            PostLimpezasRequest(roomNumber, isItClean, employeeId, cleanningDate)
+                .then((json) => {
+                    setSendRequest()
+                    console.log(json);
+                })
+                .catch((error) => console.log(error + "deu erro"));
+        }
     }, [sendRequest]);
 
     return (
         <>
             <form action="send">
-                
+
                 <div className={style.inputs}>
                     <label htmlFor="numeroQuarto" className={style.label}>
                         Número do quarto
                     </label>
                     <input
-                    type="text"
-                    name="numeroQuarto"
-                    id="numeroQuarto"
-                    className={style.inputsForm}
-                    value={roomNumber}
-                    onChange={(e) => {
-                        setRoomNumber(e.target.value);
-                    }}
-                    placeholder="Digite o numero do quarto"
-                    required
+                        type="text"
+                        name="numeroQuarto"
+                        id="numeroQuarto"
+                        className={style.inputsForm}
+                        value={roomNumber}
+                        onChange={(e) => {
+                            setRoomNumber(e.target.value);
+                        }}
+                        placeholder="Digite o numero do quarto"
+                        required
                     />
                 </div>
                 <div className={style.inputs}>
@@ -47,14 +48,14 @@ function PostLimpezas() {
                         Controle de limpeza
                     </label>
                     <input
-                    type="text"
-                    name="limpeza"
-                    id="controle"
-                    className={style.inputsForm}
-                    value={isItClean}
-                    onChange={(e) => setIsItClean(e.target.value)}
-                    placeholder="Digite a limpeza"
-                    required
+                        type="text"
+                        name="limpeza"
+                        id="controle"
+                        className={style.inputsForm}
+                        value={isItClean}
+                        onChange={(e) => setIsItClean(e.target.value)}
+                        placeholder="Digite a limpeza"
+                        required
                     />
                 </div>
                 <div className={style.inputs}>
@@ -62,44 +63,44 @@ function PostLimpezas() {
                         ID Funcionário
                     </label>
                     <input
-                    type="text"
-                    name="id_funcionario"
-                    id="id_funcionario"
-                    className={style.inputsForm}
-                    value={employeeId}
-                    onChange={(e) => setEmployeeId(e.target.value)}
-                    placeholder="Digite o Id do funcionário"
-                    required
+                        type="text"
+                        name="id_funcionario"
+                        id="id_funcionario"
+                        className={style.inputsForm}
+                        value={employeeId}
+                        onChange={(e) => setEmployeeId(e.target.value)}
+                        placeholder="Digite o Id do funcionário"
+                        required
                     />
                 </div>
                 <div className={style.container}>
                     <div className={style.inputs}>
-                    <label htmlFor="dataEntrada" className={style.label}>
-                        Data entrada
-                    </label>
-                    <input
-                        type="text"
-                        name="dataEntrada"
-                        id="dataEntrada"
-                        className={style.inputsForm}
-                        value={cleanningDate}
-                        onChange={(e) => setCleanningDate(e.target.value)}
-                        placeholder="Digite a data de entrada"
-                        required
-                    />
+                        <label htmlFor="dataEntrada" className={style.label}>
+                            Data entrada
+                        </label>
+                        <input
+                            type="text"
+                            name="dataEntrada"
+                            id="dataEntrada"
+                            className={style.inputsForm}
+                            value={cleanningDate}
+                            onChange={(e) => setCleanningDate(e.target.value)}
+                            placeholder="Digite a data de entrada"
+                            required
+                        />
                     </div>
                 </div>
 
                 <div className={style.containerButton}>
                     <button
-                    className={style.button}
-                    type="submit"
-                    onClick={(e) => {
-                        e.preventDefault();
-                        setSendRequest(true);
-                    }}
+                        className={style.button}
+                        type="submit"
+                        onClick={(e) => {
+                            e.preventDefault();
+                            setSendRequest(true);
+                        }}
                     >
-                    Cadastrar Limpeza
+                        Cadastrar Limpeza
                     </button>
                 </div>
             </form>
